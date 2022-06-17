47e025f21fad390b7afcf308278c8ece85ce2f20
chrys <chrystian.dejk@gmail.com>
2022-06-17 12:53:19 +0200

    initial commit

---
+++ src/main/java/com/unitcalculator/UnitCalculatorApplication.java
@@ -0,0 +1,15 @@
+package com.unitcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitCalculatorApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/com/unitcalculator/UnitCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
